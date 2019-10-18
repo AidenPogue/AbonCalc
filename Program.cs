d52852de68606ac5be852952f3bec005c33de120
BuildTools <unconfigured@null.spigotmc.org>
2019-10-18 10:39:08 -0700

    U mom

---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace AbonCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AbonCalc - October 2019");
+            Console.WriteLine("Enter Expression");
+            String LastInput = (Console.ReadLine());
+
+            //if (LastInput == "Quit") ;
+            //{
+            //    Console.WriteLine("Quitting");
+            //}
+            //else
+            //{
+            //    Lexer(LastInput);
+            //}
+
+            Console.WriteLine(Solver("+", 2, 3));
+
+            //Console.WriteLine(Test);
+
+            //Lexer();
+        }
+
+        static void Error(int Index, char Char)
+        {
+            Console.WriteLine("Error at " + Index + " (" + Char + ")");
+        }
+
+        int FindNextOperator(int CurrentIndex)
+        {
+
+        }
+        static void Lexer(string Input)
+        {
+            char[] InputCharArray = Input.ToCharArray();
+            //The array of the input string.
+            string[] TempArray;
+            //Array for working with current equation. It's contents are passed to the solver method and returned to be added to temptotal.
+            float TempTotal;
+            //The current result of the equation. Results from the solver method are applied to this value.
+            string CurrentValue = "";
+            //The curre nt numerical value from the array. Chars from array are added to this string, which is added to the TempArray as a float to be passed to the solver. 
+            int InputCharArrayIndex = 0;
+            //The current index that the Lexer is working at.
+
+            foreach (var CurrentChar in InputCharArray)
+            {
+                InputCharArrayIndex++;
+
+                if (CurrentChar == '(') ;
+                //Check for open bracket.
+                {
+
+                }
+
+            }
+        }
+        static float Solver(string Op, float Val1, float Val2)
+        {
+            float Output;
+        //Operator switchcase
+        switch (Op)
+            {
+                default:
+                    {
+                        Output = 0;
+                        Console.WriteLine("Invalid Operator : " + Op + ".");
+                        break;
+                    }
+
+                case "^":
+                    //Power
+                    {
+                        Output = MathF.Pow(Val1, Val2);
+                        break;
+                    }
+                case "/":
+                    //Divide
+                    {
+                        Output = Val1 / Val2;
+                        break;
+                    }
+                case "*":
+                     //Multiply
+                    {
+                        Output = Val1 * Val2;
+                        break;
+                    }
+                case "+":
+                    //Add
+                    {
+                        Output = Val1 + Val2;
+                        break;
+                    }
+                case "-":
+                    //Subtract
+                    {
+                        Output = Val1 - Val2;
+                        break;
+                    }
+            }
+
+        return Output;
+        }
+            
+
+    }
+}
